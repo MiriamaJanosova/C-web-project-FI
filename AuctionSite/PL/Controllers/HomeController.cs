46111d4b4334d19e21a9f77c5c7e872aabb3a89b
MarFridrich <mar.fridri@gmail.com>
2018-11-13 13:42:40 +0100

    MVC

---
+++ AuctionSite/PL/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Infrastructure.UnitOfWork;
+using PL.Models.Home;
+
+namespace PL.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IActionService _aus;
+
+        public HomeController(IActionService aus)
+        {
+            _aus = aus;
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(IndexModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var res = await _aus.GetAutionByName(model.Search);
+            return RedirectToAction($"Auction/{res.AuctionID}");
+
+            throw new NotImplementedException();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ AuctionSite/PL/Models/Home/IndexModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PL.Models.Home
+{
+    public class IndexModel
+    {
+
+        [Required(ErrorMessage = "Bullshit")]
+        [StringLength(10, ErrorMessage = "Kill yourself")]
+        public string Search { get; set; }
+    }
+}
